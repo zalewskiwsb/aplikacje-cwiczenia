539fe22c239c66180daf9ee236de8b5349171bf6
Mirek Zalewski <miniopl+zalewskiwsb@gmail.com>
2020-02-27 21:40:41 +0100

    Dodanie plików z ćwiczeń

---
+++ php/1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/1.css">
+  </head>
+  <body>
+    <h1>Plik PHP</h1>
+    <p><?php echo "Hello world!"; ?></p>
+  </body>
+</html>
